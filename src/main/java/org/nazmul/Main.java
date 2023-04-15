6e33c127bc88fb4959c9bc6ee0645f5e8f512da6
hoquenazmul <technazmul.hoque@gmail.com>
2023-04-15 13:28:47 -0400

    refactored mortgage calculator following OOP principals

--- src/main/java/org/nazmul/Main.java
+++ src/main/java/org/nazmul/Main.java
@@ -2,6 +2,13 @@ package org.nazmul;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int principal = (int) Console.readNumber("Principal: ", 1000, 1_000_000);
+        float annualInterest = (float) Console.readNumber("Annual Interest Rate: ", 1, 30);
+        byte years = (byte) Console.readNumber("Period (Year): ", 1, 30);
+
+        var calculator = new MortgageCalculator(principal, annualInterest, years);
+        var report = new MortgageReport(calculator);
+        report.printMortgage();
+        report.printPaymentSchedule();
     }
 }
